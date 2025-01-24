0572cd3507e0259ed741075f45ae442d730c6e10
ram sai <kramsai122000@gmail.com>
2025-01-24 20:11:56 +0530

    live port2

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
+  user: 'millets_db_user',
+  password: '(*%GyD3v$q79#FHg',
   connectionLimit: 10
 });
 
